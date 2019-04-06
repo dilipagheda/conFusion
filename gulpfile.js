f60f87c75992c9b1b0fedf23cdc605de7b43d8e8
Dilip <dilip_agheda@yahoo.com>
2019-04-06 20:57:50 +1100

    Gulp Part 2

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,13 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync'),
     del = require('del'),
-    imagemin = require('gulp-imagemin');
-
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin');
  
     gulp.task('sass', function () {
         return gulp.src('./css/*.scss')
